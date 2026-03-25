cb3fc32533327d1d28c2e77589a05d04604bb874
Shubhojit Ghosh <shubhojitghosh.005@gmail.com>
2026-03-25 13:11:21 +0530

    Updated Parser header with all function declarations1

--- include/parser.h
+++ include/parser.h
@@ -1,6 +1,10 @@
 #ifndef PARSER_H
 #define PARSER_H
+
 #include <stddef.h>
+#include <stdint.h>
+#include "../include/bencode.h"
+#include "../include/sha1.h"
 
 typedef struct
 {
@@ -26,4 +30,7 @@ typedef struct
 TorrentMeta *extract_torrent_metadata(Bencode *root);
 void print_torrent_file(TorrentMeta *meta);
 void clean_torrent_mem(TorrentMeta *meta);
+int get_info_hash(Bencode *root, uint8_t out_hash[SHA1_BLOCK_SIZE]);
+char *get_info_hash_hex(Bencode *root);
+
 #endif
